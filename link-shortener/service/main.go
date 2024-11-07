831617dfed4be6a159691b4a02ea6ced8679d9ab
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2024-11-07 12:05:03 +0330

    feat(link-shortener): Inits service

---
+++ link-shortener/service/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Link struct {
+	URL string `json:"URL"`
+}
+
+func main() {
+	http.HandleFunc("/create-link", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		link := &Link{}
+		err := json.NewDecoder(r.Body).Decode(link)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		fmt.Println("create link", link)
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != http.ErrServerClosed {
+		panic(err)
+	}
+}
