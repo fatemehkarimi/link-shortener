c32486de962c79094d566ad166567b3aa1f7da9d
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2024-12-13 17:23:56 +0330

    feat(link_shortener): Adds get link by hash

---
+++ service/structs.go
@@ -0,0 +1,21 @@
+package main
+
+import "time"
+
+type RequestCreateLink struct {
+	URL string `json:"URL"`
+}
+
+type ResponseCreateLink struct {
+	Hash       string    `json:"hash"`
+	Create_at  time.Time `json:"create_at"`
+	Expires_at time.Time `json:"expires_at"`
+}
+
+type RequestGetURLByHash struct {
+	Hash string `json:"hash"`
+}
+
+type ResponseGetURLByHash struct {
+	URL *string `json:"URL"`
+}
